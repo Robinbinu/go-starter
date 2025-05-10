1a40abdd61e20f38c9ecde98f00799d532681290
Zodsenpai <91971225+Robinbinu@users.noreply.github.com>
2025-05-10 16:16:11 +0000

    Refactor input handling and calculation logic in profit_calcutator.go

--- profit_calcutator.go
+++ profit_calcutator.go
@@ -1,24 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main(){
 	var revenue,expenses,tr,EBT,profit,ratio float64
 
-	fmt.Print("Enter revenue: ")
-	fmt.Scan(&revenue)
-	fmt.Print("Enter expenses: ")
-	fmt.Scan(&expenses)
-	fmt.Print("Enter tax rate: ")
-	fmt.Scan(&tr)
+	revenue = getInput("Enter revenue: ")
+	expenses = getInput("Enter expenses: ")
+	tr = getInput("Enter tax rate: ")
 
-	EBT = revenue - expenses
-	profit = EBT * (1 - tr/100)
-	ratio = profit/EBT
+	calculateVals(revenue,expenses,tr)
 
 	fmt.Println(EBT)
 	fmt.Println(profit)
 	fmt.Println(ratio)
 
+}
 
+func getInput(text string) (userInput float64){
+	fmt.Print(text)
+	fmt.Scan(&userInput)
+	return
+}
+
+func calculateVals(revenue,expenses,tr float64)(ebt,profit,ratio float64){
+	ebt = revenue - expenses
+	profit = ebt * (1 - tr/100)
+	ratio = profit/ebt
+	return
 }
