ba68874a9f28c227d7035d05b177d28f8afacdf6
Robinbinu <91971225+Robinbinu@users.noreply.github.com>
2025-05-11 21:10:56 +0530

    Implement file read/write functionality and update project structure

--- app.go
+++ app.go
@@ -1,17 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var filePath = "data.txt"
 
-// func main(){ 
-// 	b:=0
-// 	s:="wow"
-// 	var temp float32
-// 	fmt.Scan(&temp)
-// 	fmt.Scan(&b)	
-// 	fmt.Scan(&s)
-// 	fmt.Println(b)
-// 	fmt.Print(s)
-// }
+func writeToFile(data string){
+	os.WriteFile(filePath,[]byte(data),0644)
+}
 
+func readFromFile(){
+	byteData, _ := os.ReadFile(filePath)
+	text := string(byteData)
+	num, _ := strconv.ParseInt(text,10,64)
+	fmt.Println(num)
+}
+
+func main(){
+	var val string;
+
+	fmt.Print("Enter a value :")
+	fmt.Scan(&val)
+
+	writeToFile(val)
+	readFromFile()
+
+}
 
--- for_loop.go
+++ for_loop.go
@@ -1,11 +1,11 @@
-package main
+// package main
 
-import 
+// import 
 
-func main1(){
-	value := 1
-	 for value<=10 {
-		fmt.Println(value)
-		value++
-	 }
-}
+// func main1(){
+// 	value := 1
+// 	 for value<=10 {
+// 		fmt.Println(value)
+// 		value++
+// 	 }
+// }
