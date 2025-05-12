dd60f9476ac6703645ba7fb99078bc163d65c6cd
Zodsenpai <91971225+Robinbinu@users.noreply.github.com>
2025-05-12 05:32:41 +0000

    Refactor main function: comment out file operations and add pointer demonstration

--- app.go
+++ app.go
@@ -2,25 +2,32 @@ package main
 
 import (
 	"fmt"
-	"example.com/fileops"
+	// "example.com/fileops"
 )
 
 
 
 func main(){
 
+	age := 32
+	
+	agePointer := &age //pointer referencing the address of age
+	fmt.Println(agePointer," ",&age) //output: 0xc000010100   0xc000010100
+	ageFromPointer := *agePointer //dereferencing the value from the pointer
+	fmt.Println(ageFromPointer," ",*agePointer) //output: 32   32
+
 	// panic("First panic")
 
-	var val string;
+	// var val string;
 
-	fmt.Print("Enter a value :")
-	fmt.Scan(&val)
+	// fmt.Print("Enter a value :")
+	// fmt.Scan(&val)
 
-	fileops.WriteToFile(val)
-	err:=fileops.ReadFromFile()
-	if err != nil{
-		panic(err)
-	}
+	// fileops.WriteToFile(val)
+	// err:=fileops.ReadFromFile()
+	// if err != nil{
+	// 	panic(err)
+	// }
 
 }
 
