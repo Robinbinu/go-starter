38d5ce5c4640e5344972208314fae0421693227c
Zodsenpai <91971225+Robinbinu@users.noreply.github.com>
2025-05-12 05:44:02 +0000

    Refactor main function: simplify pointer usage and remove commented-out code

--- app.go
+++ app.go
@@ -5,16 +5,18 @@ import (
 	// "example.com/fileops"
 )
 
+func incrementAgeBy10(age *int){ //func requires a pointer param 
+	*age = *age + 10 //pointer dereferenced and mutated
+}
 
 
 func main(){
 
 	age := 32
-	
-	agePointer := &age //pointer referencing the address of age
-	fmt.Println(agePointer," ",&age) //output: 0xc000010100   0xc000010100
-	ageFromPointer := *agePointer //dereferencing the value from the pointer
-	fmt.Println(ageFromPointer," ",*agePointer) //output: 32   32
+	fmt.Println(age) //output: 32
+	incrementAgeBy10(&age)
+	fmt.Println(age) //output: 42
+
 
 	// panic("First panic")
 
