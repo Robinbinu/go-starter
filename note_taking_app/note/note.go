59ccbc726a7a18bc1451757aa46136c01b229e8c
Zodsenpai <91971225+Robinbinu@users.noreply.github.com>
2025-05-13 15:52:07 +0000

    Refactor Note struct and methods: update field names for consistency and change file saving format to JSON

--- note_taking_app/note/note.go
+++ note_taking_app/note/note.go
@@ -1,36 +1,45 @@
 package note
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 )
 
-type Note struct{
-	title string
-	content string
-	timeCreated time.Time
+type Note struct {
+	Title       string
+	Content     string
+	TimeCreated time.Time
 }
 
-func New(title,content string)(Note,error){
-	if title == "" || content == ""{
-		return Note{},errors.New("invalid input")
+func New(title, content string) (Note, error) {
+	if title == "" || content == "" {
+		return Note{}, errors.New("invalid input")
 	}
 
 	return Note{
-		title: title,
-		content: content,
-		timeCreated: time.Now(),
-	},nil
+		Title:       title,
+		Content:     content,
+		TimeCreated: time.Now(),
+	}, nil
 }
 
-func (note Note) Display(){
-	fmt.Printf("Title: %v\nContent: %v\nCreated on:%v\n",note.title,note.content,note.timeCreated)
+func (note Note) Display() {
+	fmt.Printf("Title: %v\nContent: %v\nCreated on: %v\n", note.Title, note.Content, note.TimeCreated)
 }
 
-func (note Note) Save() error{
-	noteText := fmt.Sprintf("%v\n%v\nCreated on: %v",note.title,note.content,note.timeCreated)
-	return os.WriteFile("Note.txt",[]byte(noteText),0644)
-}
+func (note Note) Save() error {
+	fileName := strings.ReplaceAll(note.Title, " ", "_")
+	fileName = strings.ToLower(fileName)
+	fileName = fileName + ".json"
 
+	jsonData, err := json.Marshal(note)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(fileName, jsonData, 0644)
+}
