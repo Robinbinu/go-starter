211629bc27a18dcb18c4567d5c75ff448c172e09
Zodsenpai <91971225+Robinbinu@users.noreply.github.com>
2025-05-13 15:35:59 +0000

    implemented note saving to txt file

--- note_taking_app/app.go
+++ note_taking_app/app.go
@@ -1,30 +1,56 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
+
 	"example.com/note/note"
 )
 
-
-func main(){
-	newNote,_ := getNoteData()
-	newNote.PrintNoteDetails()
-}
-
-func getNoteData()(newNote note.Note,err error){
-	title:=getUserInput("Note title: ")
-	content:=getUserInput("Note content: ")
-	newNote,err= note.New(title,content)
-	if err != nil{
-		return note.Note{},err
+func errorFound(err error) bool{
+	if err != nil {
+		fmt.Println(err)
+		return true
 	}
-	return newNote,nil
+	return false
 }
 
+func main() {
+	newNote, err := getNoteData()
+	if errorFound(err){return}
 
+	newNote.Display()
 
-func getUserInput(text string) (value string){
+	err = newNote.Save()
+	if errorFound(err){return}
+	fmt.Println("note saved successfully")
+
+}
+
+func getNoteData() (newNote note.Note, err error) {
+	title,_ := getUserInput("Note title: ")
+	content,_ := getUserInput("Note content: ")
+	newNote, err = note.New(title, content)
+	if err != nil {
+		return note.Note{}, err
+	}
+	return newNote, nil
+}
+
+func getUserInput(text string) (value string,err error) {
 	fmt.Print(text)
-	fmt.Scanln(&value)
-	return value
+	// fmt.Scanln(&value) commented because Scan often gets messy with strings separated
+	reader := bufio.NewReader(os.Stdin)
+	value,err = reader.ReadString('\n')
+	if err != nil{
+		return "",err
+	}
+
+	value = strings.TrimSuffix(value,"\n") //if not added the strings has a new line by default
+	value = strings.TrimSuffix(value,"\r") //windows ends strings with \r\n
+
+	return value,nil
+
 }
--- note_taking_app/note/note.go
+++ note_taking_app/note/note.go
@@ -3,6 +3,7 @@ package note
 import (
 	"errors"
 	"fmt"
+	"os"
 	"time"
 )
 
@@ -24,9 +25,12 @@ func New(title,content string)(Note,error){
 	},nil
 }
 
-func (note Note) PrintNoteDetails(){
-	fmt.Println(note.title)
-	fmt.Println(note.content)
-	fmt.Println(note.timeCreated)
+func (note Note) Display(){
+	fmt.Printf("Title: %v\nContent: %v\nCreated on:%v\n",note.title,note.content,note.timeCreated)
+}
+
+func (note Note) Save() error{
+	noteText := fmt.Sprintf("%v\n%v\nCreated on: %v",note.title,note.content,note.timeCreated)
+	return os.WriteFile("Note.txt",[]byte(noteText),0644)
 }
 
