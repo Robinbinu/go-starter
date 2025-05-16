a34b7ffe2a838db9749b37931d86af410eaaf2e3
Zodsenpai <91971225+Robinbinu@users.noreply.github.com>
2025-05-16 03:30:01 +0000

    Add initial implementation of lists module with example array usage

---
+++ arrays/lists.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+    var arr = []int{1,2,3,4} //alt decl: var arr = [4]int{}
+    strArr := []string{"str1","str2","str3"}
+
+   fmt.Println(arr) //[1 2 3 4]
+   fmt.Println(strArr[1]) //str2
+}
