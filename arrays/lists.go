a4224ba68b5fff4878004bb417d7f9f526502f8a
Zodsenpai <91971225+Robinbinu@users.noreply.github.com>
2025-05-16 03:38:44 +0000

    Add array slicing examples to lists module

--- arrays/lists.go
+++ arrays/lists.go
@@ -8,4 +8,9 @@ func main(){
 
    fmt.Println(arr) //[1 2 3 4]
    fmt.Println(strArr[1]) //str2
+
+   slicedArr := arr[1:3] //here [a:b] elements from a->b-1 are sliced
+   slicedArr1 := arr[:3] //[a:]
+   fmt.Print(slicedArr)
+   fmt.Print(slicedArr1)
 }
