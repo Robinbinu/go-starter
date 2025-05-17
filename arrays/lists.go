dd6c2d1a0b2f96b66fb2786b918613b2c86a517b
Zodsenpai <91971225+Robinbinu@users.noreply.github.com>
2025-05-17 03:31:19 +0000

    Enhance lists module: add length and capacity calculations, and improve output formatting for sliced arrays

--- arrays/lists.go
+++ arrays/lists.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main(){
     var arr = []int{1,2,3,4} //alt decl: var arr = [4]int{}
+    n := len(arr) //len of array - no of elements present
+    c := cap(arr) //capacity the array can hold
     strArr := []string{"str1","str2","str3"}
 
    fmt.Println(arr) //[1 2 3 4]
@@ -11,6 +13,9 @@ func main(){
 
    slicedArr := arr[1:3] //here [a:b] elements from a->b-1 are sliced
    slicedArr1 := arr[:3] //[a:]
+
    fmt.Print(slicedArr)
-   fmt.Print(slicedArr1)
+   fmt.Println(slicedArr1)
+   fmt.Println(n)
+   fmt.Println(c)
 }
