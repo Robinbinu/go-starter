11cb556b76686fa6d29e0063306fa97ff1c61f84
Zodsenpai <91971225+Robinbinu@users.noreply.github.com>
2025-05-21 13:57:04 +0000

    Implement transformer functions and variadic function for note-taking app; moved note-taking app files to practice_projects dir

--- functions/func.go
+++ functions/func.go
@@ -11,6 +11,16 @@ func main() {
 	numbers[0] = 3
 	fmt.Println(transformNumbers(numbers,getTransformFn(numbers))) //[9 6 9 12]
 	fmt.Println(transformNumbers(numbers,func(i int) int {return i*4})) //[12 8 12 16]
+	
+	//factory function
+	double := createTransformer(2) //creates a number doubler func
+	fmt.Println(transformNumbers(numbers,double))
+	
+	fourTime := createTransformer(4)
+	fmt.Println(transformNumbers(numbers,fourTime))
+
+	//variadic function test
+	sumUp("Life",2,3,45,34,1,123,343)
 }
 
 func transformNumbers(nums []int, transform transformedFn) []int {
@@ -36,4 +46,18 @@ func double(num int) int {
 
 func triple(num int) int {
 	return num * 3
+}
+
+func createTransformer(factor int) func(int)int{
+	return func(number int) int {
+		return number*factor
+}
+}
+
+ //variadic function
+func sumUp(text string,numbers ...int){  //sumUp("Life",2,3,45)
+	fmt.Println(text)
+	for _,v := range numbers{
+		fmt.Println(v)
+	}
 }
